a9c6224ea6acfbb9ee91bc5994d332041d1710b4
jclarkDM <jclark@develomark.com>
2026-05-20 12:27:36 -0400

    Changed auth middleware to use basic auth for deploy keys to keep track of source (deploy ID)

--- packages/server/src/auth-middleware.ts
+++ packages/server/src/auth-middleware.ts
@@ -3,11 +3,14 @@ import z from "zod";
 import { payloadSchema } from "./schemas";
 import { verify } from "hono/jwt";
 import { env } from "cloudflare:workers";
+import { auth as parseBasicAuth } from "hono/utils/basic-auth";
+import { timingSafeEqual } from "hono/utils/buffer";
 
 interface Env {
   Variables: {
     payload: {
-      type: "deploy-key"
+      type: "deploy-key",
+      id: string
     } | {
       type: "jwt",
       payload: z.output<typeof payloadSchema>
@@ -19,19 +22,26 @@ export const auth = createMiddleware<Env>(async (c, next) => {
   const authorization = c.req.header("Authorization");
   if(!authorization) return c.text("Unauthorized", 401);
 
-  const bearer = authorization.split("Bearer").at(-1)?.trim();
-  if(!bearer) return c.text("Unauthorized", 401);
-
-  if(bearer === env.API_SECRET) {
-    c.set("payload", { type: "deploy-key" });
-    return await next();
+  const basic = parseBasicAuth(c.req.raw);
+  let bearer: string | undefined = undefined;
+  if(authorization.toLowerCase().startsWith("bearer")) {
+    bearer = authorization.split(" ").at(-1);
   }
 
-  try {
-    const result = await verify(bearer, env.API_SECRET, "HS256");
-    c.set("payload", { type: "jwt", payload: result as z.output<typeof payloadSchema> });
+  if(basic) {
+    const isEqual = await timingSafeEqual(env.API_SECRET, basic.password);
+    if(!isEqual) return c.text("Unauthorized", 401);
+
+    c.set("payload", { type: "deploy-key", id: basic.username });
+
     return await next();
-  } catch(e) {
-    return c.text("Unauthorized", 401);
+  } else if(bearer) {
+    try {
+      const result = await verify(bearer, env.API_SECRET, "HS256");
+      c.set("payload", { type: "jwt", payload: result as z.output<typeof payloadSchema> });
+      return await next();
+    } catch(e) {}
   }
+
+  return c.text("Unauthorized", 401);
 });
