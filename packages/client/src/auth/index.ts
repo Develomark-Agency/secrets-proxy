5c6a12080fd1450471d75b29cd949d9c4eb101c1
jclarkDM <jclark@develomark.com>
2026-05-27 13:30:14 -0400

    Added `loadCredentialsWithAutoRefresh` export

--- packages/client/src/auth/index.ts
+++ packages/client/src/auth/index.ts
@@ -1,3 +1,3 @@
-export { loadCredentials } from "./credentials";
+export { loadCredentials, loadCredentialsWithAutoRefresh } from "./credentials";
 export { login } from "./login";
 export { refresh } from "./refresh";
